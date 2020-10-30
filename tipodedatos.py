86e34ff286346fd1a73054ecfa8d13c338864e94
luisantonyhg <63163461+luisantonyhg@users.noreply.github.com>
2020-10-30 09:52:01 -0500

    primer avance

---
+++ tipodedatos.py
@@ -0,0 +1,38 @@
+# TIPO DE DATOS
+
+#STRING O CADENA DE TEXTO
+
+texto_doble = "Luis Antony Huamani Gonzales"
+texto_simple = 'El mejor programador de Python'
+
+
+#INT NUMERICO O ENTERO
+
+numero = 5
+
+#BOLEANO O VERDADERO O FALSO
+
+boleano = True
+boleano = False
+
+# FLOAT O DECIMALES
+
+decimales = 4.5
+
+
+#CONCATENACION DE TEXRO
+
+luis = "Antony"
+yuliana = "stefani"
+print(luis+yuliana)
+print(luis+", " + yuliana)
+
+#MULTIPLICACION DE TEXTO
+
+multiplicar = "carlos"
+print(multiplicar*3)
+
+
+
+
+
